1666539dc23a0e43d9488bfc90b30f3b61e8572b
BALAVIGNESHWARTG <ashwinhari06@gmail.com>
2026-07-06 14:49:13 +0530

    Reset solutions to boilerplate templates for self-implementation

--- Easy/two_sum/solution.py
+++ Easy/two_sum/solution.py
@@ -6,11 +6,5 @@ Link: https://leetcode.com/problems/two-sum/
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        num_to_index = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-            num_to_index[num] = i
-        return []
+        pass
 
